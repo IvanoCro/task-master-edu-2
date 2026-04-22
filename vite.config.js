f7266f702a887139aef2cd1ce9c22e1f8764cbba
IvanoCro <zirdumivano3@gmail.com>
2026-04-22 10:33:32 +0200

    fix vite base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/task-master-edu/'
+  base: '/task-master-edu-2/' 
 })
